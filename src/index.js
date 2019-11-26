9b62de80be9b71fe27868112aca8f4170e1297b0
torcoste <torcoste@gmail.com>
2019-11-26 17:33:12 +0300

    app init

---
+++ src/index.js
@@ -0,0 +1,24 @@
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
+import {name as appName} from '../app.json';
+import 'react-native-gesture-handler';
+import {createAppContainer} from 'react-navigation';
+import {createStackNavigator} from 'react-navigation-stack';
+
+import Home from './screens/Home';
+import Details from './screens/Details';
+
+const AppNavigator = createStackNavigator(
+  {
+    Home: Home,
+    Details: Details,
+  },
+  {
+    initialRouteName: 'Home',
+  },
+);
+
+AppRegistry.registerComponent(appName, () => createAppContainer(AppNavigator));
